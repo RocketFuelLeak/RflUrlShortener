324172e6f370b6b175e6e6af1848d91389ca1b6a
Adam Hellberg <sharparam@sharparam.com>
2014-07-11 10:50:06 +0200

    Update server for production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 set :rails_env, 'production'
 
-server 'rfl.im', user: 'rails', roles: %w{web app db},
+server 'prod.rfl.im', user: 'rails', roles: %w{web app db},
     ssh_options: {
         port: 2553
     }
